022d718e4ef623130142a0d5e5e0e2113635d5a2
efiegel <efiegel01@gmail.com>
2024-08-03 13:07:43 -0500

    existing formatting fixes

--- tests/integration/test_audio.py
+++ tests/integration/test_audio.py
@@ -7,8 +7,12 @@ class TestAudio:
     @pytest.fixture
     def transcriber(self):
         return Audio("tiny")
-    
+
     def test_transcribe(self, transcriber):
         audio_sample_filepath = "tests/sample_data/audio/audio_sample_1.wav"
         transcription = transcriber.transcribe(audio_sample_filepath)
-        assert transcription["text"].lstrip().startswith("The boy was there when the sun rose.")
+        assert (
+            transcription["text"]
+            .lstrip()
+            .startswith("The boy was there when the sun rose.")
+        )
