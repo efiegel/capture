5d7af0551aa227b7fe033860650bd0d00eb3ca58
efiegel <efiegel01@gmail.com>
2024-08-10 10:50:58 -0500

    prepare update-notes for daily notes

--- capture/notes.py
+++ capture/notes.py
@@ -1,11 +1,26 @@
 import os
+from datetime import datetime
 
 
 class Notes:
     def __init__(self, notes_directory: str) -> None:
         self.notes_directory = notes_directory
 
-    def create_from_text_file(self, text_file_path: str):
+    def get_or_create_daily_note(self) -> str:
+        file_name = f"{datetime.now().strftime('%Y-%m-%d')}.md"
+        file_path = f"{self.notes_directory}/{file_name}"
+        if not os.path.exists(file_path):
+            self.write("", file_path)
+        return file_path
+
+    def update_note(self, file_path, content):
+        pass
+
+    def write(self, content: str, note_path: str):
+        with open(note_path, "w") as f:
+            f.write(content)
+
+    def add_content(self, text_file_path: str):
         with open(text_file_path, "r") as f:
             content = f.read()
 
@@ -13,7 +28,3 @@ class Notes:
         note_file_name = file_name.replace(".txt", ".md")
         note_path = f"{self.notes_directory}/{note_file_name}"
         self.write(content, note_path)
-
-    def write(self, content: str, note_path: str):
-        with open(note_path, "w") as f:
-            f.write(content)
--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-scripts=("sync" "transcribe insert_note")
+scripts=("sync" "transcribe update_notes")
 for script in "${scripts[@]}"; do
     python -m "scripts.$script" &
 done
