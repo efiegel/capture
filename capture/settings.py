cb602de528a5a2db4c0a38b77ebc12d046891558
efiegel <efiegel01@gmail.com>
2024-08-15 07:27:16 -0500

    organize settings file better

--- capture/settings.py
+++ capture/settings.py
@@ -4,10 +4,15 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-FILE_DIRECTORY = os.getenv("TRANSCRIPTION_DIRECTORY")
-NOTES_DIRECTORY = os.path.expanduser(os.getenv("NOTES_DIRECTORY"))
-FOOD_LOG_PATH = os.path.expanduser(os.getenv("FOOD_LOG_PATH"))
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+# Audio directories
 SOURCE_AUDIO_DIRECTORY = os.path.expanduser(os.getenv("SOURCE_AUDIO_DIRECTORY"))
 RAW_AUDIO_DIRECTORY = os.getenv("RAW_AUDIO_DIRECTORY")
 TRANSCRIPTION_DIRECTORY = os.getenv("TRANSCRIPTION_DIRECTORY")
+
+# Note directories
+FILE_DIRECTORY = os.getenv("TRANSCRIPTION_DIRECTORY")
+NOTES_DIRECTORY = os.path.expanduser(os.getenv("NOTES_DIRECTORY"))
+FOOD_LOG_PATH = os.path.expanduser(os.getenv("FOOD_LOG_PATH"))
+
+# Secrets
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
