249fb955e83b6e37e3801f5c79b89b8d6f3664e2
efiegel <efiegel01@gmail.com>
2024-08-21 20:45:14 -0500

    fix up flakiness of settings test

--- tests/unit/test_settings.py
+++ tests/unit/test_settings.py
@@ -1,7 +1,10 @@
+import importlib
+
+from capture import settings
+
+
 class TestSettings:
     def test_setting_with_expanduser_is_none_if_not_set(self, monkeypatch):
         monkeypatch.setenv("SOURCE_AUDIO_DIRECTORY", "")
-
-        from capture.settings import SOURCE_AUDIO_DIRECTORY
-
-        assert SOURCE_AUDIO_DIRECTORY is None
+        importlib.reload(settings)
+        assert settings.SOURCE_AUDIO_DIRECTORY is None
