cc7743dddb97ae9e7a42fd2ee34d2d24f3cd7ad3
efiegel <efiegel01@gmail.com>
2024-09-02 10:46:51 -0500

    fill out agent class

--- capture/llm/agent.py
+++ capture/llm/agent.py
@@ -1,2 +1,40 @@
+from typing import List, Type, Union
+
+from langchain_openai import ChatOpenAI
+from pydantic import BaseModel, create_model
+
+from .chains import CategorizerChain, IntegratorChain, ParserChain
+
+
 class Agent:
-    pass
+    def __init__(self, model_name: str):
+        self.model = ChatOpenAI(model=model_name)
+
+    def categorize(self, content: str, categories: list[str]) -> str:
+        chain = CategorizerChain(self.model)
+        inputs = {"content": content, "categories": categories}
+        response = chain.invoke(inputs)
+        return response.content
+
+    def integrate(self, existing_content: str, new_content: str):
+        chain = IntegratorChain(self.model)
+        inputs = {"existing_content": existing_content, "new_content": new_content}
+        response = chain.invoke(inputs)
+        return response.content
+
+    def parse(
+        self,
+        content: str,
+        response_format: Union[Type[BaseModel], Type[list[BaseModel]]],
+    ):
+        if response_format.__class__ == BaseModel.__class__:
+            chain = ParserChain(self.model, response_format)
+            return chain.invoke({"content": content})
+        else:
+            items_model = self._create_items_model(response_format)
+            chain = ParserChain(self.model, items_model)
+            return chain.invoke({"content": content}).items
+
+    @staticmethod
+    def _create_items_model(obj: Type[list[BaseModel]]):
+        return create_model("Items", items=(List[obj.__args__[0]], ...))
