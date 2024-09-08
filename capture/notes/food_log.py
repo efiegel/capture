f6cb08be2f26748d5143e3ae9b07f70aa743f4b8
efiegel <efiegel01@gmail.com>
2024-09-07 19:54:21 -0500

    completely drop FoodLogEntry schema

--- capture/notes/food_log.py
+++ capture/notes/food_log.py
@@ -4,23 +4,11 @@ from itertools import islice
 from pydantic import BaseModel
 
 
-class FoodLogEntry(BaseModel):
-    time: str
-    name: str
-    qty: float
-    unit: str
-    calories: float
-    protein_grams: float
-    fat_grams: float
-    carbs_grams: float
-    sodium_mg: float
-
-
 class FoodLog:
     def __init__(self, path: str):
         self.path = path
 
-    def add_entries(self, entries: list[FoodLogEntry]):
+    def add_entries(self, entries: list[BaseModel]):
         with open(self.path, mode="a", newline="") as file:
             writer = csv.writer(file)
             for entry in entries:
