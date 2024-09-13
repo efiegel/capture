532efb55db07e41ff00e1e9d090e2bca2a609ad4
efiegel <efiegel01@gmail.com>
2024-09-12 22:24:09 -0500

    delete extraneous settings

--- capture/settings.py
+++ capture/settings.py
@@ -4,15 +4,12 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# Audio directories
+# Audio directory
 SOURCE_AUDIO_DIRECTORY = (
     os.path.expanduser(os.getenv("SOURCE_AUDIO_DIRECTORY", "")) or None
 )
-RAW_AUDIO_DIRECTORY = os.getenv("RAW_AUDIO_DIRECTORY")
-TRANSCRIPTION_DIRECTORY = os.getenv("TRANSCRIPTION_DIRECTORY")
 
 # Note directories
-FILE_DIRECTORY = os.getenv("TRANSCRIPTION_DIRECTORY")
 NOTES_DIRECTORY = os.path.expanduser(os.getenv("NOTES_DIRECTORY", "")) or None
 
 # Vectorstore
