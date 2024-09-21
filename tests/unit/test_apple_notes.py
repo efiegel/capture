7400e5ffc0b00f725e3b1d00cf0b9628344d04d5
efiegel <efiegel01@gmail.com>
2024-09-20 19:36:23 -0500

    connect to apple notes in a unit test for debugging

---
+++ tests/unit/test_apple_notes.py
@@ -0,0 +1,26 @@
+from ScriptingBridge import SBApplication
+
+
+def test_apple_notes():
+    apple_notes_app = SBApplication.applicationWithBundleIdentifier_("com.apple.Notes")
+    folder_name = "capture"
+    account_name = "iCloud"
+
+    notes = []
+    # can also fetch the default account at apple_notes_app.accounts()[0]
+    for account in apple_notes_app.accounts():
+        if account.name() == account_name:
+            folders = account.folders()
+            for folder in folders:
+                if folder.name() == folder_name:
+                    notes = folder.notes()
+                    break
+            break
+
+    if notes:
+        latest_note = notes[0]
+        print("id:", latest_note.id())
+        print("name:", latest_note.name())
+        print("body:", latest_note.body())
+        print("creation date:", latest_note.creationDate())
+        print("modification date:", latest_note.modificationDate())
