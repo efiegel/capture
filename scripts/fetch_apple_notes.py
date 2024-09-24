57147fc220b709910c3c6259b1a1a08ba47786f7
efiegel <efiegel01@gmail.com>
2024-09-23 20:48:55 -0500

    fetch apple notes script

---
+++ scripts/fetch_apple_notes.py
@@ -0,0 +1,25 @@
+import os
+
+from dotenv import load_dotenv
+
+from capture.sources.apple_notes import AppleNotes
+from capture.vault import Vault
+
+load_dotenv()
+
+VAULT_DIRECTORY = os.path.expanduser(os.getenv("VAULT_DIRECTORY", ""))
+
+
+if __name__ == "__main__":
+    vault = Vault(VAULT_DIRECTORY)
+    apple_notes_source = AppleNotes("capture")
+    for note in apple_notes_source.note_iterator():
+        print("-" * 40)
+        print(f"title: {note.title}")
+        print(f"created at: {note.creation_date}")
+
+        user_input = input("  >> add to vault? (y/n) [y]: ").strip().lower() or "y"
+        if user_input == "n":
+            break
+
+        vault.add(note.content)
